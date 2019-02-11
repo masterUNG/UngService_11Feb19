e863ce322205c9f7b419ceac1316984d905af65f
masterUNG <phrombutr@gmail.com>
2019-02-11 13:37:11 +0700

    About Me Success

--- UngService11Feb/ViewController.swift
+++ UngService11Feb/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    Explicit
+    let name: String = "Master UNG"
+    let surname: String = "Phrombutr"
+    var address = ["53", "Bangna14", "Bangkok", "10260"]
+    var addressDic = ["email":"master@abc.com", "phone":"081-8595309"]
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var surnameLabel: UILabel!
+        
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var phoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+//        Show View
+        nameLabel.text = "Name: \(name)"
+        surnameLabel.text = "Surname: \(surname)"
+        addressLabel.text = "Address: \(address[0]) \(address[1]) \(address[2]) \(address[3])"
+        emailLabel.text = addressDic["email"]
+        phoneLabel.text = addressDic["phone"]
+        
+    }   // Main Method
 
 
-}
+}   // Main Class
 
